b3f54db7f0617a98d3ab4ba0c8f43a76d52fe412
Daniel <acdaniel051@gmail.com>
2026-04-09 00:03:32 +0000

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char tecla_pressionada;
+    printf("A\n");
+    printf("-----------------------------\n"); 
+    printf("isso aqui eh uma string\n");
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numero_inteiro;
+    numero_inteiro = 10;
+    numero_inteiro = -5;
+    numero_inteiro = 00004;
+    printf("10, -5, 00004 \n");
+
+    return 0;
+}
